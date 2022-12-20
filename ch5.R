3a5082c6974b46a8d633383a2f366e81894420cb
Yuya Matsumura <mattu.yuya@gmail.com>
2022-12-20 11:18:00 +0900

    本編に「サポートサイトを参照」とした部分を追加

--- ch5.R
+++ ch5.R
@@ -13,11 +13,11 @@ ames_cv_splits <- vfold_cv(ames_train,
                            v = 10)
 
 ames_rec <-
-  recipe(Sale_Price ~ ., data = ames_train) %>%
-  step_log(Sale_Price, base = 10) %>%
-  step_YeoJohnson(Lot_Area, Gr_Liv_Area) %>%
-  step_other(Neighborhood, threshold = .1)  %>%
-  step_zv(recipes::all_predictors())
+  recipes::recipe(Sale_Price ~ ., data = ames_train) %>%
+  recipes::step_log(Sale_Price, base = 10) %>%
+  recipes::step_YeoJohnson(Lot_Area, Gr_Liv_Area) %>%
+  recipes::step_other(Neighborhood, threshold = .1)  %>%
+  recipes::step_zv(recipes::all_predictors())
 
 # ワークフローの設定
 ames_rf_cv <-
@@ -36,9 +36,8 @@ ames_rf_cv <-
 rf_params <-
   list(trees(),
        mtry() %>%
-         dials::finalize(ames_rec %>% prep() %>% bake() %>% select(!Sale_Price))) %>%
+         dials::finalize(ames_rec %>% prep() %>% bake(new_data = NULL) %>% select(!Sale_Price))) %>%
   dials::parameters()
-
 rf_params
 
 rf_grid_range <-
@@ -46,6 +45,16 @@ rf_grid_range <-
   dials::grid_random(size = 5)
 
 rf_grid_range
+
+# ランダムサーチの実行
+ames_rf_grid <-
+  ames_rf_cv %>%
+  tune::tune_grid(resamples = ames_cv_splits,
+                  grid = rf_grid_range,
+                  control = tune::control_grid(save_pred = TRUE),
+                  metrics = yardstick::metric_set(rmse))
+
+autoplot(ames_rf_grid)
 
 # 最適なパラメータを選択
 ames_rf_grid_best <-
@@ -96,4 +105,34 @@ ames_rf_bayes_best <-
   ames_rf_bayes %>%
   tune::show_best()
 ames_rf_bayes_best
+
+# 5.3.2 ベイズ最適化によるハイパーパラメータチューニング(書籍に記載をしていない部分)
+# 選んだパラメータでモデル作成
+ames_rf_bayes_best <-
+  parsnip::rand_forest(
+    # 最適なパラメータを選択
+    # 1行目を選択
+    trees = ames_rf_bayes_best$trees[1],
+    mtry = ames_rf_bayes_best$mtry[1]
+  ) %>%
+  parsnip::set_engine("ranger",
+                      seed = 71) %>%
+  parsnip::set_mode("regression")
+
+# ワークフローの更新
+ames_rf_cv_bayes_last <-
+  ames_rf_cv %>%
+  workflows::update_model(ames_rf_bayes_best)
+
+# 更新したワークフローで訓練データ全体にモデル適用
+ames_rf_bayes_last_fit <-
+  ames_rf_cv_bayes_last %>%
+  tune::last_fit(split_ames_df)
+
+# 最終的な精度を算出
+last_rmse_bayes <- ames_rf_bayes_last_fit %>%
+  tune::collect_metrics()
+
+last_rmse_bayes
+
 
